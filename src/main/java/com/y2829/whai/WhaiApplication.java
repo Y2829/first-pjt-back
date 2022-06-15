626a8e91a832d1c01ca32651aff4169b9ae15354
heung27 <wlsgmd1535@naver.com>
2022-06-15 22:01:55 +0900

    initial commit

---
+++ src/main/java/com/y2829/whai/WhaiApplication.java
@@ -0,0 +1,13 @@
+package com.y2829.whai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/y2829/whai/WhaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2829.whai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
