442f2bb2e1403e8fc66d7e0dfd9fbc83bf0b16dc
heung27 <wlsgmd1535@naver.com>
2022-06-19 14:25:05 +0900

    feat: write initial user controller

--- src/main/java/com/y2829/whai/controller/UserRestController.java
+++ src/main/java/com/y2829/whai/controller/UserRestController.java
@@ -3,7 +3,9 @@ package com.y2829.whai.controller;
 import com.y2829.whai.common.utils.ApiUtils;
 import com.y2829.whai.dto.UserDto;
 import com.y2829.whai.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +20,43 @@ public class UserRestController {
     private final UserService userService;
 
     @GetMapping("login")
+    @Operation(summary = "로그인", description = "로그인합니다.")
     public ApiUtils.ApiResult<UserDto.Response> login() {
         return success(
                 null
         );
     }
+
+    @GetMapping("logout")
+    @Operation(summary = "로그아웃", description = "로그아웃합니다.")
+    public ApiUtils.ApiResult<UserDto.Response> logout() {
+        return success(
+                null
+        );
+    }
+
+    @GetMapping
+    @Operation(summary = "회원 조회", description = "회원을 조회합니다.")
+    public ApiUtils.ApiResult<UserDto.Response> findUser() {
+        return success(
+                null
+        );
+    }
+
+    @GetMapping
+    @Operation(summary = "회원 정보 수정", description = "회원 정보를 수정합니다.")
+    public ApiUtils.ApiResult<UserDto.Response> modifyUser() {
+        return success(
+                null
+        );
+    }
+
+    @DeleteMapping
+    @Operation(summary = "회원 탈퇴", description = "회원을 탈퇴합니다.")
+    public ApiUtils.ApiResult<UserDto.Response> removeUser() {
+        return success(
+                null
+        );
+    }
+
 }
